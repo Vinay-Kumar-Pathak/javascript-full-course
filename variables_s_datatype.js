797a8545699764e2f1e1a7c729e27395e3cd81d9
Vinay-Kumar-Pathak <vinaypathakvp31@gmail.com>
2025-05-15 16:51:51 +0530

    first commit

---
+++ variables_s_datatype.js
@@ -0,0 +1,38 @@
+console.log('vinay pathak');
+console.log('leade');
+console.log('alone');
+//variables:variables are container for data
+//it have case sensitive "a","A" both are different
+let name ="vinay";
+let age=18;
+let total_income =0;
+//var is used in before 2015 .variable can be redeclare
+//so be used let due to new feature
+//const is always fix it cannot be changed.
+{
+    a=5
+    console.log(a);
+}
+{
+    a=7
+    console.log(a);
+}
+
+//student profile
+const student={
+    name: 'vinay',
+    age:18,
+    cgpa:75,
+
+}
+console.log(student);
+
+
+//first question:product information
+const product={
+   title: "ball pen",
+   rating: 4,
+   offers:5,
+   price: 270,
+}
+console.log(product);
